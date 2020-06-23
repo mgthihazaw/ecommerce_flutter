d2b3352f5b14a0c798cbd49367563989d8b28ea3
Your NameThihazaw <mgthihazaw777@gmail.com>
2020-06-23 15:11:20 +0630

    update carousel with pretty design

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -2,37 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:carousel_slider/carousel_options.dart';
 
-class Carousel extends StatelessWidget {
+class Carousel extends StatefulWidget {
+  @override
+  _CarouselState createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  int _current;
+  List _imageList = [
+    "assets/carousels/image1.jpg",
+    "assets/carousels/image2.jpg",
+    "assets/carousels/image3.jpg",
+    "assets/carousels/image4.jpg",
+    "assets/carousels/image5.jpg",
+  ];
+  List<T> mapSlide<T>(List list, handler) {
+    List<T> result = [];
+    int i = 0;
+    list.forEach((value) {
+      result.add(handler(i, value));
+      i++;
+    });
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-        options: CarouselOptions(
-          height: 170,
-          aspectRatio: 16 / 10,
-          viewportFraction: 1,
-          initialPage: 0,
-          enableInfiniteScroll: true,
-          reverse: false,
-          autoPlay: true,
-          autoPlayInterval: Duration(seconds: 3),
-          autoPlayAnimationDuration: Duration(milliseconds: 800),
-          autoPlayCurve: Curves.fastOutSlowIn,
-          enlargeCenterPage: true,
-          scrollDirection: Axis.horizontal,
+    // print(_current);
+    return Column(
+      children: <Widget>[
+        CarouselSlider(
+          options: CarouselOptions(
+              height: 170,
+              aspectRatio: 16 / 8,
+              viewportFraction: 1,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 1500),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (index, _) {
+                setState(() {
+                  _current = index;
+                });
+              }),
+          items: _imageList.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration:
+                        BoxDecoration(color: Theme.of(context).accentColor),
+                    child: Image.asset(i, fit: BoxFit.cover));
+              },
+            );
+          }).toList(),
         ),
-        items: [1, 2, 3, 4, 5].map((i) {
-          return Builder(
-            builder: (BuildContext context) {
+        Container(
+          decoration: BoxDecoration(color: Colors.grey[300]),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: mapSlide<Widget>(_imageList, (index, url) {
               return Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                decoration: BoxDecoration(color: Theme.of(context).accentColor),
-                child: Image.asset("assets/carousels/image$i.jpg",
-                    fit: BoxFit.cover),
+                margin: EdgeInsets.all(7),
+                width: _current == index ?10 :5,
+                height: _current == index ?10 :5,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Theme.of(context).primaryColor),
+                    shape: BoxShape.circle,
+                    color: _current == index
+                        ? Theme.of(context).primaryColor
+                        : null),
               );
-            },
-          );
-        }).toList(),
-      );
+            }),
+          ),
+        )
+      ],
+    );
   }
 }
